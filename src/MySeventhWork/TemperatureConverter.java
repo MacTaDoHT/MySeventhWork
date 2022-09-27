f07ba65ee6894320abcc06a394cbb14776c50da5
MacTaDoHT <93607990+MacTaDoHT@users.noreply.github.com>
2022-09-27 20:51:40 +0300

    Выполнено седьмое домашнее задание. Это было жестко.

---
+++ src/MySeventhWork/TemperatureConverter.java
@@ -0,0 +1,13 @@
+package MySeventhWork;
+
+public class TemperatureConverter {
+
+    public static double FahrenheitCelsius(double value) {
+
+        double result = (value - 32) / 1.8;
+
+        result = Math.round(result * 10);
+        return result / 10;
+    }
+
+}
